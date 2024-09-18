04950d4c3b589ff0878acf6ccbeba1e165d38262
Kandyce <knhopkinson23@gmail.com>
2024-09-18 13:50:54 -0400

    finished loop basics

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
 // Arrays are not primitive value types
 // They are reference types
-const fruits = ['apple', 'orange','kiwi' ,'grape'];
+const fruits = ['apple', 'orange','kiwi' ,'grape', 'banana', 'blueberry', 'strawberry', 'plantain'];
 const data = ['Kandyce', "Mbua", 35, ['pickleball', 'fishing']];
 
-console.log(fruits);
-
-fruits [2] = 'strawberry';
-
-console.log(fruits);
-
-// Increase the data age number by one = console.log the original value and the new value after you increase it
-
-console.log(data[2]);
-
-data[2] ++;
-
-console.log(data[2]);
-
+for (let index = 0; index < fruits.length; index ++) {
+console.log(fruits[index]);
+}
