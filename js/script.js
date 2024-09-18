52979d4a1ca821a40770cc1565b80f76de4010f1
Kandyce <knhopkinson23@gmail.com>
2024-09-18 11:55:56 -0400

    finished conditional overview

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 const firstName = "Kandyce";
-let age = 44;
+let age = 18;
 
-// age = age +1;
-// age += 5;
-// age-+5;
-// age *=5;
-// age =age +1; is the same as below
-age ++;
+// If age is greater than 20 and less than 50, console log 'you are young'
 
-console.log(age);
+// IF age is greater than or equal to 50, console log 'you are getting older'
+
+// If age is less than 20 and greater than 12, console log 'you are a teenager'
+
+if (age > 20 && age < 50) {
+    console.log("you are young");
+} else if (age >= 50){
+    console.log ("you are  getting older");
+} else if (age <20 && age >12) {
+    console.log("you are a teenager");
+} else{
+    console.log("your age is not within our range");
+}
