99ee127cf32e78ea4873d95e375f642d8474f310
Kandyce <knhopkinson23@gmail.com>
2024-09-19 12:39:33 -0400

    finished function basics

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 const names = ['Izzy', 'Matt', 'City', 'Kandyce'];
 
 function printNames() {
+    const someVal = 'this is a val'
     for (let index = 0; index < names.length; index++) {
         console.log(names[index]);
     }
 }
 
-printNames();
+function add(num1, num2) {
+console.log(num1+num2);
+}
+
+function add(num1,num2){
+return num1 + num2;
+}
+
+const result = add(3,10);
+
+console.log(result);
+
+// add(10,5);
+// add(20,100);
+// add(50,5);
+
+// function printVals(str,bool,num, arr){console.log(arr[1]);
+// }
+
+// printVals ('some string', true, 15, ['Michael', 'Nina', 'Roman']);
