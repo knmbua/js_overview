d39280f6c28e7317e1ffcfb397cc84292970bb09
Kandyce <knhopkinson23@gmail.com>
2024-09-20 14:33:17 -0400

    finished web api basics

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
-const userData = {
-    name: 'JD',
-    age: 44,
-    phone: '777-777-7777',
-    address: '555 coding st',
-    info: {
-        location: 'Atlanta area',
-        hobbies: ['pickleball', 'fishing']
-    },
+const header = document.querySelector('h1');
+const paragraph = document.querySelector('p:nth-child(2)');
+const image = document.querySelector('#main-image');
+const noteOutput = document.querySelector('.output');
+const timerDisplay=document.querySelector('#time');
+const timerBtn=document.querySelector('#start-btn');
 
-    printAge: function () {
-        console.log(this.age);
-    },
-    haveBirthday: function () {
-        this.age++;
-        console.log("Happy Birthday");
-    },
-    addHobby: function (hobby) {
-        // Need to reference an argument that is passed to addHobby (string of a hobby)
-        this.info.hobbies.push(hobby);
-    },
-    // Push the argument(hobby) to the info.hobbies array
-    printHobbies: function() {
-        // console.log the hobbies array
-        console.log(this.info.hobbies);
-        // BONUS - Loop over the hobbies array and console.log each hobby
-        for (let i = 0; i < this.info.hobbies.length; i++) {
-            console.log(this.info.hobbies[i]);
+header.classList.add('crazy');
+
+header.style.textDecoration = 'underline';
+
+// image.setAttribute('src', 'https://images.pexels.com/photos/28056731/pexels-photo-28056731/free-photo-of-a-plate-with-a-bagel-and-coffee-on-it.jpeg')
+
+// inserting a new article element into the outputsection
+noteOutput.insertAdjacentHTML('beforeend', `
+    <article>
+    <h3>New note text<h3>
+    <p> Added On: 9/20/2024</p>
+  </article>
+    `);
+
+    // Timers
+    // setInterval(function(){
+    //     console.log('time up!');
+    // }, 3000);
+let count = 10;
+let started = false;
+
+    
+
+    timerBtn.addEventListener('click', function (){
+        if(!started) {
+            const timer = setInterval(function(){
+                count--;
+        
+                timerDisplay.innerText = 'Time: ' +count;
+        
+                if (count<=0) {
+                    clearInterval(timer);
+                    timerDisplay.innerText = 'Time: 10';
+                    count = 10;
+                    started=false;
+                }
+            }, 1000);
+
+            started=true;
         }
-    }
-}
+    });
 
-userData.addHobby('traveling');
-userData.printHobbies()
+    // CREATE A BUTTON THAT RESETS THE TIMER.. HIT THE TIMER
